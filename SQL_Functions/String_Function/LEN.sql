41c3fc46c6f7de8b86f012dd2bd97797a614e82e
nshinde3910829 <68653931+nshinde3910829@users.noreply.github.com>
2025-08-20 12:03:29 +0530

    Create LEN.sql
    this file contain LEN function of the SQL

---
+++ SQL_Functions/String_Function/LEN.sql
@@ -0,0 +1,12 @@
+select
+first_name,
+len(first_name) as length_of_char
+from customer
+
+select
+
+'9876543210' as register_phone,
+
+left('9876543210', 2) as FirstNo,
+
+right('9876543210', 2) as LastNo
